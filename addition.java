517ea979795e74b32ec5c0f96b5727bea5a86ea2
Tarun Dubey <tarun2019dubey@gmail.com>
2025-01-07 13:45:50 +0530

    initial commit

--- addition.java
+++ addition.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class addition {
-    public static void main(System[] args){
+    public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
         System.out.println("enter the first number");
         int x= sc.nextInt();
--- subtract.java
+++ subtract.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class subtract {
     
-    public static void main(System[] args){
+    public static void main(String[] args){
 
         Scanner sc= new Scanner(System.in);
         
